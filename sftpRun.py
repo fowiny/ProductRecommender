74e6e6e2780ddfb1dd6ed51dbe4596cd436c2d9d
Festo Owiny <festo.owiny@studenti.unitn.it>
2019-04-14 18:39:16 +0200

    Update sftpRun.py

--- sftpRun.py
+++ sftpRun.py
@@ -4,8 +4,8 @@ sftpConnect = pysftp.Connection(host="xxx", username="xxx", private_key="zzz", p
 
 data = sftpConnect.listdir('/home/converse/recommendationJson/')
 
-print ('Does path /home/converse/recommendationJson/ exisit ? :', sftpConnect.lexists('/home/converse/recommendationJson/')) #Tests if path exisits
-print ('Does file personalizedRecommend_RP.json exist ? :', sftpConnect.lexists('/home/converse/recommendationJson/personalizedRecommend_RP.json')) #Tests if file exists
-print ('Files on path /home/converse/recommendationJson/ are:', sftpConnect.listdir('/home/converse/recommendationJson/')) # List files in the given path
+print ('Does path /xx/ exisit ? :', sftpConnect.lexists('/xx/')) #Tests if path exisits
+print ('Does file personalizedRecommend_RP.json exist ? :', sftpConnect.lexists('/xx/personalizedRecommend_RP.json')) #Tests if file exists
+print ('Files on path /xx/recommendationJson/ are:', sftpConnect.listdir('/xx/recommendationJson/')) # List files in the given path
 
 sftpConnect.close()
